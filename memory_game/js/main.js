19212631be8d7436c5b6b1df48afbb2c5b19f89d
CarsonButterfield <47901139+CarsonButterfield@users.noreply.github.com>
2019-09-04 13:04:22 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,12 @@
-var cardOne = "queen"
-var cardTwo = "queen"
-var cardThree = "king"
-var cardFour = "king"
+const cards = ["king","king","queen","queen"];
+var cardsInPlay = [];
+var cardOne = cards[0]
+var cardTwo = cards[1]
+cardsInPlay.push(cardOne)
+cardsInPlay.push(cardTwo)
+console.log(`user flipped ${cardOne} and ${cardTwo}`)
+
+if(cardOne === cardTwo){
+  alert('Its a match!')
+}
+else{alert("wrong, idiot")}
