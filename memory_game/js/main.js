aaaf8209393084c23dc5c8718ea909e183b8bb71
CarsonButterfield <47901139+CarsonButterfield@users.noreply.github.com>
2019-09-06 09:47:24 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,21 @@
-const cards = ["king","king","queen","queen"];
+const cards = [
+  {
+    rank:"king",
+    suit:"hearts",
+    cardImage:"images/king-of-hearts.png"
+},{
+  rank:"king",
+  suit:"diamonds",
+  cardImage:"images/king-of-diamonds.png"
+},{
+  rank:"queen",
+  suit:"hearts",
+  cardImage:"images/queen-of-hearts.png"
+},{
+  rank:"queen",
+  suit:"diamonds",
+  cardImage:"images/queen-of-diamonds.png"
+}];
 var cardsInPlay = [];
 
 
@@ -9,8 +26,8 @@ function checkMatch(){
   else{alert("wrong, idiot")}
 cardsInPlay = []}
 function flipCard(cardid){
-  cardsInPlay.push(cards[cardid])
-  console.log(`user flipped ${cards[cardid]}`)
+  cardsInPlay.push(cards[cardid].rank)
+  console.log(`user flipped ${cards[cardid].rank} of ${cards[cardid].suit} ${cards[cardid].cardImage}`)
 if(cardsInPlay.length === 2){checkMatch()}
 }
 flipCard(0)
